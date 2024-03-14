4dad5ee13f01e96f650d55a51b9363341a6123f8
javeriagithub <163407795+javeriagithub@users.noreply.github.com>
2024-03-15 01:14:20 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,41 @@
+let username1 = "javeria";
+let pswrd = "123";
+let code1 = "456";
+
+if (username1 =="javeria" && pswrd == "123") {
+//    console.log("kindly provide code");
+}
+    if(code1 == "456"){
+      console.log("welcome home" + "" +username1)
+        console.log("invalid code")
+}
+
+        let username = "tooba";
+        let pswrd2
+        let code = "456"; 
+        if(username =="tooba" && pswrd2 == "000") {
+              console.log("kindly provide code");
+            }
+             if(code == "456"){
+                 console.log("welcome home" + "" +username );
+             }
+            
+          
+            else if(username =="tooba" && pswrd2 == "000") {
+                console.log("kindly provide code");
+              }
+               
+            
+            else{
+                
+            console.log("invalid user")
+            }
+            
+
+            
+        
+        
+                 
+            
+        
+    
